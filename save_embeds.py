b2083e51f0a62de16b75726ffb1f5762abc6cc78
Miriam Shiffman <meereeum@gmail.com>
2017-01-11 18:35:31 -0500

    extract embeddings (np array) from saved model OR convert saved embeddings for use with tf projector

--- save_embeds.py
+++ save_embeds.py
@@ -3,11 +3,11 @@ import sys
 
 import numpy as np
 
-from model import LDA2Vec
 
-
-def save_embeddings(meta_graph, outdir=None):
+def embeddings_from_meta_graph(meta_graph, outdir=None, tf_projector=False):
 	"""Load trained model and save embeddings"""
+	from model import LDA2Vec
+
 	outdir = ("results_{}".format(os.path.basename(meta_graph))
 			  if outdir is None else outdir)
 
@@ -25,7 +25,48 @@ def save_embeddings(meta_graph, outdir=None):
 
 		np.save(os.path.join(outdir, f), arr)
 
+
+def tf_projector_from_embeddings(f_embed, metadata="metadata.tsv", outdir=None):
+	"""Load np array of trained embeddings and save for visualization with TensorBoard Projector"""
+	import tensorflow as tf
+	from tensorflow.contrib.tensorboard.plugins import projector
+
+	outdir = (os.path.join(os.path.dirname(os.path.abspath(f_embed)), "out")
+			  if outdir is None else outdir)
+
+	# tf graph for embeddings
+	embeds = tf.Variable([47], dtype=tf.float32, name="embeddings")
+	embed_vals = tf.placeholder(tf.float32, [None, None])
+	assign_embeds = tf.assign(embeds, embed_vals, validate_shape=False)
+
+	sesh = tf.Session()
+	sesh.run(tf.global_variables_initializer())
+	logger = tf.summary.FileWriter(outdir, sesh.graph)
+
+	# tf projector
+	config = projector.ProjectorConfig()
+	embedding = config.embeddings.add()
+	embedding.tensor_name = embeds.name
+	embedding.metadata_path = os.path.join(outdir, metadata)
+	projector.visualize_embeddings(logger, config)
+
+	# assign embeddings and save
+	feed_dict = {embed_vals: np.load(f_embed)}
+	sesh.run(assign_embeds, feed_dict=feed_dict)
+
+	saver = tf.train.Saver(tf.global_variables())
+	outfile, _ = os.path.splitext(os.path.basename(f_embed))
+	saver.save(sesh, os.path.join(outdir, outfile))
+
+	logger.flush()
+	logger.close()
+
+
 if __name__ == "__main__":
-	meta_graph = sys.argv[1]
-	assert os.path.exists(meta_graph + "meta"), "Where's your infile ?"
-	save_embeddings(meta_graph)
+	infile = sys.argv[1]
+
+	# assert os.path.exists(infile + "meta"), "Where's your infile ?"
+	# save_embeddings(meta_graph=infile)
+
+	assert os.path.exists(infile), "Where's your infile ?"
+	tf_projector_from_embeddings(f_embed=infile)
